eb3358167e0627ead2e90e23934178d9ef7dfea8
ganeswar-velvadapu <ganeshvelvadapu@gmail.com>
2025-07-11 23:29:03 +0530

    pulled everything from main

---
+++ src/app/esummit/page.js
@@ -0,0 +1,11 @@
+import HeroSection from "../components/homeComponents/HeroSection";
+import Caution from "../components/homeComponents/Caution";
+
+export default function ESummitPage(){
+    return (
+        <>
+            <Caution />
+            <HeroSection text="Comming Soon"/>
+        </>
+    )
+}
