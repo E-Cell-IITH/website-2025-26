d71c8581ee20a3a01dc06f3e54eb33c2447beb8d
ganeswar-velvadapu <ganeshvelvadapu@gmail.com>
2025-07-16 03:25:25 +0530

    removed caution from esummit page

--- src/app/esummit/page.js
+++ src/app/esummit/page.js
@@ -1,11 +1,9 @@
 import EsummitHeroSection from "@/components/esummit/comingsoon";
-import Caution from "@/components/homeComponents/Caution";
 
 
 export default function ESummitPage(){
     return (
         <>
-            <Caution />
             <EsummitHeroSection/>
         </>
     )
