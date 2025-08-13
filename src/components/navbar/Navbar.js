405e19ab316ef39fa3dbb6f98bfa83947dd6e7a7
ganeswar-velvadapu <ganeshvelvadapu@gmail.com>
2025-08-13 11:59:52 +0530

    added scroll bar and removed py

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -47,8 +47,8 @@ export default function Navbarlayout() {
         >
           <span
             className={`absolute w-8 h-[3px] bg-white top-1/2 left-1/2 transition-all duration-400 ease-out rounded-full ${isOpen
-                ? 'rotate-45 -translate-x-1/2 -translate-y-1/2 bg-gradient-to-r from-blue-400 to-cyan-400'
-                : '-translate-x-1/2 -translate-y-3 group-hover:w-9'
+              ? 'rotate-45 -translate-x-1/2 -translate-y-1/2 bg-gradient-to-r from-blue-400 to-cyan-400'
+              : '-translate-x-1/2 -translate-y-3 group-hover:w-9'
               }`}
           />
           <span
@@ -57,8 +57,8 @@ export default function Navbarlayout() {
           />
           <span
             className={`absolute w-8 h-[3px] bg-white top-1/2 left-1/2 transition-all duration-400 ease-out rounded-full ${isOpen
-                ? '-rotate-45 -translate-x-1/2 -translate-y-1/2 bg-gradient-to-r from-blue-400 to-cyan-400'
-                : '-translate-x-1/2 translate-y-3 group-hover:w-9'
+              ? '-rotate-45 -translate-x-1/2 -translate-y-1/2 bg-gradient-to-r from-blue-400 to-cyan-400'
+              : '-translate-x-1/2 translate-y-3 group-hover:w-9'
               }`}
           />
         </button>
@@ -74,15 +74,14 @@ export default function Navbarlayout() {
 
       {/* Nav Links Container */}
       {showLinks && (
-        <div className="fixed inset-0 z-50 flex items-center justify-center overflow-hidden">
-          <div className="relative flex flex-col items-center max-h-[80vh] overflow-y-auto py-20 space-y-10 pointer-events-auto scrollbar-hide px-8">
+        <div className="fixed inset-0 z-50 flex items-center justify-center overflow-y-auto">
+          <div className="relative flex flex-col items-center max-h-[80vh] space-y-10 pointer-events-auto px-8 w-full">
             {/* Background Blobs */}
             <div className="absolute inset-0 pointer-events-none">
               <div className="absolute top-20 left-20 w-40 h-40 bg-blue-500/5 rounded-full blur-3xl animate-pulse"></div>
               <div className="absolute bottom-20 right-20 w-60 h-60 bg-cyan-400/5 rounded-full blur-3xl animate-pulse delay-1000"></div>
               <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-80 h-80 bg-purple-500/5 rounded-full blur-3xl animate-pulse delay-500"></div>
             </div>
-
             {/* Navigation Items */}
             {navItems.map((item, index) => (
               <div key={item.href + index}>
@@ -92,15 +91,12 @@ export default function Navbarlayout() {
                   className="group relative block text-4xl text-white tracking-wider hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r hover:from-blue-400 hover:via-cyan-400 hover:to-purple-400 transition-all duration-300 ease-out hover:scale-110 hover:translate-x-4"
                 >
                   <span className="relative z-10">{item.label}</span>
-
                   {/* Hover glow effect */}
                   <div className="absolute inset-0 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
                     <div className="absolute inset-0 bg-gradient-to-r from-blue-400/20 to-cyan-400/20 blur-xl rounded-lg transform scale-110"></div>
                   </div>
-
                   {/* Underline */}
                   <div className="absolute -bottom-2 left-0 w-0 h-1 bg-gradient-to-r from-blue-400 to-cyan-400 group-hover:w-full transition-all duration-400 ease-out rounded-full"></div>
-
                 </Link>
               </div>
             ))}
